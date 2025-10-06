b55f6c0f5d170c9c23cd24f492cf2d0b827ad47f
stap780 <panaet80@mail.ru>
2025-10-06 20:37:53 +0300

    update.view

--- app/services/list_items_json_generator_service.rb
+++ app/services/list_items_json_generator_service.rb
@@ -62,7 +62,7 @@ class ListItemsJsonGeneratorService
   end
 
   def s3_file_key(client)
-    "list_#{@account.id}_client_#{@external_client_id}_list_items.json"
+    "lists/list_#{@account.id}_client_#{@external_client_id}_list_items.json"
   end
 end
 
