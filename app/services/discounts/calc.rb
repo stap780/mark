2618460b80b9d7e8336bb9af3baeb80f657699f5
stap780 <panaet80@mail.ru>
2025-10-21 14:50:32 +0300

    add discount and user

--- app/services/discounts/calc.rb
+++ app/services/discounts/calc.rb
@@ -4,11 +4,12 @@ module Discounts
       new(account: account, data: data).call
     end
 
-    def initialize(account:, data:)
-      @account = account
-      @data = data.respond_to?(:with_indifferent_access) ? data.with_indifferent_access : data
-      add_lower_price
-    end
+  def initialize(account:, data:)
+    @account = account
+    @data = data.respond_to?(:with_indifferent_access) ? data.with_indifferent_access : data
+    add_lower_price
+    add_collections
+  end
 
     def call
       Rails.logger.info "Discounts::Calc - processing for account #{@account.id}"
@@ -78,6 +79,53 @@ module Discounts
       
       prices = @data['order_lines'].map { |line| line['sale_price'] }.compact
       @data['lower_price'] = prices.min if prices.any?
+    end
+
+    # Добавляем коллекции товаров в данные
+    def add_collections
+      return unless @data['order_lines'].present?
+      
+      @data['order_lines'].each do |line|
+        # Получаем коллекции товара через API Insales
+        colls = get_product_collections(line['product_id'])
+        line['colls'] = colls
+      end
+    end
+
+    # Получаем коллекции товара через API Insales
+    def get_product_collections(product_id)
+      return [] unless product_id.present?
+      
+      begin
+        # Получаем данные товара через API Insales
+        insales_api = @account.insales.first
+        return [] unless insales_api.present?
+        
+        # Инициализируем API
+        insales_api.api_init
+        
+        # Получаем товар из Insales
+        ins_product = InsalesApi::Product.find(product_id)
+        
+        # Получаем коллекции товара
+        collection_ids = ins_product.try(:collection_ids) || []
+        
+        # Получаем названия коллекций
+        colls = collection_ids.map do |id|
+          begin
+            collection = InsalesApi::Collection.find(id)
+            collection.try(:title)
+          rescue => e
+            Rails.logger.error "Error getting collection #{id}: #{e.message}"
+            nil
+          end
+        end.compact
+        
+        colls
+      rescue => e
+        Rails.logger.error "Error getting collections for product #{product_id}: #{e.message}"
+        []
+      end
     end
   end
 end
