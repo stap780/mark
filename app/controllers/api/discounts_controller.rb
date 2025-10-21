3d4f2125f827948cdaa68bcd91eaaaffa5a5a80d
stap780 <panaet80@mail.ru>
2025-10-21 14:39:09 +0300

    add discount and user

--- app/controllers/api/discounts_controller.rb
+++ app/controllers/api/discounts_controller.rb
@@ -1,6 +1,8 @@
 class Api::DiscountsController < ApplicationController
   # Отключаем CSRF защиту для API endpoints
   skip_before_action :verify_authenticity_token
+  # Отключаем аутентификацию для API endpoints
+  skip_before_action :require_authentication
   
   # POST /api/accounts/:account_id/discounts/calc
   # Принимает полную структуру данных заказа из Insales
