87f6c609a491ebc25e8f0da3100e90f3c99b50ee
stap780 <panaet80@mail.ru>
2025-12-25 13:56:29 +0300

    update destroy incase

--- app/models/incase.rb
+++ app/models/incase.rb
@@ -5,6 +5,7 @@ class Incase < ApplicationRecord
   belongs_to :client
 
   has_many :items, dependent: :destroy
+  has_many :automation_messages
 
   enum :status, {
     new: "new",
@@ -19,6 +20,7 @@ class Incase < ApplicationRecord
   # Генерируем порядковый номер для отображения пользователю
   # (number используется для номеров из API/InSales и может быть строковым)
   before_create :generate_display_number, unless: :display_number?
+  before_destroy :check_automation_messages_dependency
   after_update_commit :trigger_update_events
 
   def self.ransackable_attributes(auth_object = nil)
@@ -58,6 +60,13 @@ class Incase < ApplicationRecord
   end
 
   private
+
+  def check_automation_messages_dependency
+    return unless automation_messages.exists?
+
+    errors.add(:base, I18n.t('incases.destroy.has_automation_messages'))
+    throw(:abort)
+  end
 
   def generate_display_number
     # Генерируем порядковый номер для отображения пользователю
