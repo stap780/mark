11ce57700b96dccde911de9b465c76aa087f8a28
stap780 <panaet80@mail.ru>
2026-01-22 13:37:38 +0300

    update

--- config/initializers/billing.rb
+++ config/initializers/billing.rb
@@ -15,12 +15,12 @@ module Billing
     def initialize
       @providers = {
         paymaster: {
-          merchant_id: Rails.application.credentials.dig(:paymaster, :merchant_id),
-          secret: Rails.application.credentials.dig(:paymaster, :secret),
-          base_url: Rails.application.credentials.dig(:paymaster, :base_url) || "https://paymaster.ru",
-          success_url: Rails.application.credentials.dig(:paymaster, :success_url),
-          fail_url: Rails.application.credentials.dig(:paymaster, :fail_url),
-          result_url: Rails.application.credentials.dig(:paymaster, :result_url)
+          # merchant_id: Rails.application.credentials.dig(:paymaster, :merchant_id),
+          # secret: Rails.application.credentials.dig(:paymaster, :secret),
+          # base_url: Rails.application.credentials.dig(:paymaster, :base_url) || "https://paymaster.ru",
+          # success_url: Rails.application.credentials.dig(:paymaster, :success_url),
+          # fail_url: Rails.application.credentials.dig(:paymaster, :fail_url),
+          # result_url: Rails.application.credentials.dig(:paymaster, :result_url)
         },
         invoice: {
           # Настройки для счетов
